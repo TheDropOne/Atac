af67b48640fb8eb9a45c3a2abf1767ec5b8fbc0f
TheDropOne <TheDropOne@gmail.com>
2017-05-20 11:34:26 +0300

    Задание 7. Сделано полностью

--- app.js
+++ app.js
@@ -4,6 +4,11 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+
+app.get('/index', function (req, res) {
+    res.send('Hello World!');
+});
+
 app.listen(2727, function() {
     console.log('Example app listening on port 2727!')
 });
